e4b040af98ef6b94f2633e3deb59f4c016d92260
Fredrik Ljungqvist <fljungqvist@gmail.com>
2021-03-24 07:46:26 +0100

    Working

--- bot.js
+++ bot.js
@@ -1,31 +1,24 @@
-class Player{
-    constructor (){
-
-
-        
-    }
-} 
+ 
 
  class Bot {
     constructor(name){
     this.name=name
     }
-    /* makeGuessTime(guess){
-       return setTimeout(guess,  Math.floor(Math.random() * 4500) + 2000)
-    } */
+    // not used at the moment
+     makeGuessTime(guess){
+       return setTimeout(guess,  Math.floor(Math.random() * 4500) + 1000)
+    } 
 }
 
-let highestLowNumber = 12
-let lowestHighNumber = 16
-let answer=""
+
 class SmartBot extends Bot{
     constructor(){
         super("smartBot")
     }
-    guess(){
-        // return super.makeGuessTime(()=>(highestLowNumber + Math.floor(Math.random() * 
-        // (lowestHighNumber - highestLowNumber) + 1))) //Istället för console.log ange en div där värdet skall renderas
-        return highestLowNumber + Math.floor(Math.random() * (lowestHighNumber - highestLowNumber) + 1)
+    //Makes smart guess
+    guess(highestLowNumber, lowestHighNumber){
+    
+        return highestLowNumber + (Math.floor(Math.random() * (lowestHighNumber - highestLowNumber)) + 1)
 
     }
 
@@ -36,41 +29,14 @@ class DumbBot extends Bot{
     constructor(){
         super("dumbBot")
     }
+    //Makes dumb guess
     guess(){
-        // return super.makeGuessTime(()=>(Math.floor(Math.random() * 20) + 1)) //Istället för console.log ange en div där värdet skall renderas
-       return Math.floor(Math.random() * 20) + 1
+       
+        return Math.floor(Math.random() * 20) + 1
        
     }
-    
-
-
 } 
-/* let test = new DumbBot()
-test.guess() */
- /* let test2 = new DumbBot()
- let test3 = new SmartBot()
- test2.guess
- test3.guess()
-
- console.log(test3.guess) */
- 
 
  
- 
-
-
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-
-  
-  async function sample() {
-    console.log('a');
-    console.log('waiting...')
-    let delayres = await sleep(3000);
-    console.log('b');
-  }
-  
 
 
--- game.js
+++ game.js
@@ -1,153 +1,143 @@
-//En klass om har hand om data för hela omgången
-
 
 function init() {
     
 }
-
-function startGame(){
-    let smartBot = new SmartBot
-    let dumbBot = new DumbBot
+//starts the game and gets values, values not used for now
+function startGame(){   
+    let name="Fredrik"
     let player = document.getElementById("playerName").value
     let guess = document.getElementById("playerInput").value
-    let game = new GameState("olle")
+    let game = new GameState(name)
     
         
-       
-    console.log(game.correctNumb)
+    console.log(game.players)
+    console.log("Correct number= " +game.correctNumb)
     
 }
 
 
 
  class GameState{
-    players=["olle", "dumbBot", "smartBot"]
-    playerName="olle"
+    players=["dumbBot", "smartBot"]
+    playerName=""
     highestLowNumber=1
     lowestHighNumber=20
     playerGuesses=0
     correctNumb=this.correctNumb
-    round
-    winner
+    winner=null
     dumbBot = new DumbBot
-    smartBot = new SmartBot 
-    
-    
-    
+    smartBot = new SmartBot
+    numberOfGuesse=0
+    playerGuess=null
     win=0
-    rounds
+    
     constructor(playerName){
         this.playerName=playerName
         this.correctNumb=this.correctNumber()
-        
-
-        
-        
     }
+    //Calculates the correct number and saves it to correctNumb
     correctNumber(){
         console.log("number")
         let nr = Math.random() * (20 - 1) + 1
         let correctNr = Math.floor(nr)
+        this.addUser(this.playerName)
         this.gameLoop()
         return correctNr
     }
+    //Adds player to players array
+    addUser(playerName){
+        this.players.unshift(playerName)
+    }
     
-
-   async gameLoop(){
+    //Main loop to run game
+    async gameLoop(){
+       
         for (let index = 0; index < this.players.length; index++) {
-           
+            console.log(this.numberOfGuesse)
+            alert(this.players[index])
             this.timer()
             await this.pause()
             
              if (this.playerName!==this.players[index])
             {   
                 if (this.players[index]=="smartBot") {
+                    let playerGuess=this.smartBot.guess(this.highestLowNumber, this.lowestHighNumber)
+                    document.getElementById("smartBotOutput").innerHTML=playerGuess
                     
-                    document.getElementById("smartBotOutput").innerHTML=this.smartBot.guess()
-                    let playerGuess=this.getGuess("smartBotOutput")
-                    let vinst = this.guessCheck(playerGuess, this.correctNumb)
-                    if (vinst==1) {
-                        console.log("i win" +this.players[index])
-                        this.winner=this.players[index]
-                        winner = this.winner
-                        stats()
-                        break
-                        /* alert("Du har vunnit " + this.players[index]) */
-                    }
+                    this.playerGuess=playerGuess
+                    console.log(this.playerGuess+ " smartBot")
+                 
 
                 }else{
-                    console.log(this.dumbBot.guess())
-                    document.getElementById("dumbBotOutput").innerText=this.dumbBot.guess()
-                    let playerGuess = this.getGuess("dumbBotOutput")
-
-                    let vinst = this.guessCheck(playerGuess, this.correctNumb)
-                    if (vinst==1) {
-                        console.log("i win" +this.players[index])
-                        this.winner=this.players[index]
-                        winner = this.winner
-                        stats()
-                        break
-                        /* alert("Du har vunnit " + this.players[index]) */
-                    }
+                    let playerGuess = this.dumbBot.guess()
+                    document.getElementById("dumbBotOutput").innerText=playerGuess
+                    this.playerGuess=playerGuess
+                    console.log(this.playerGuess+ " dumbBot")
+                   
                 }
                 
             }else{
-                this.getGuess("playerInput")
-                let playerGuess=this.getGuess("plyerInput")
-                let vinst = this.guessCheck(playerGuess, this.correctNumb)
-                if (vinst==1) {
-                    console.log("i win" +this.players[index])
-                    this.winner=this.players[index]
-                    winner = this.winner
-                    stats()
-                    break
-                    
-                }
-            
-            /* let playerGuess = this.getGuess("playerInput")
-            console.log(playerGuess)
-            let vinst = this.guessCheck(playerGuess, this.correctNumb)
-            console.log(vinst)
-            if (vinst==1) {
-                console.log("i win" +this.players[index]) */
-                this.winner=this.players[index]
-                let winner = this.winner
-                this.stats()
-                break
-                /* alert("Du har vunnit " + this.players[index]) */
+                
+                let playerGuess=this.getGuess("playerInput")
+                this.playerGuess=playerGuess
+                this.numberOfGuesse++
             }
+
+            
+            let win = this.guessCheck(this.playerGuess, this.correctNumb)
+                if (win==1) {
+                    alert("I win " +this.players[index])
+                    this.winner=this.players[index]
+                    let winner = this.winner
+                    console.log(this.winner)
+                    console.log(this.players[index]+" Have guessed " + this.numberOfGuesse + " times to win!")
+                    this.stats()
+                    return
+                }
+            await sleep(2000)
+            
+            console.log(this.playerGuess)
+            if (this.playerGuess>this.correctNumb && this.playerGuess<this.lowestHighNumber)
+            {
+                this.lowestHighNumber=Number(this.playerGuess)
+            }if (this.playerGuess<this.correctNumb && this.playerGuess>this.highestLowNumber) {
+                this.highestLowNumber = Number(this.playerGuess)
+            } 
+            console.log(this.highestLowNumber, this.lowestHighNumber)
+            
             
         }
-        await sleep(3000)
-        console.log("after last sleep")
         this.gameLoop()
-    /* this.gameLoop(); */
+    
     }
-
+    //Not in use, solved with other functions
     win(){
         if (win=1){
             alert("du har vunnit")
             stats()
         }
     }
-
+    //Saves winner to local storage
     stats() {
+        let winners=[]
         winners = JSON.parse(localStorage.getItem("winners"))
         if (winners !=null) {
-            storage.setItem(winners, winner);
+            storage.setItem(winners, this.winner);
         }else{
-            winners.push(winner)
+            
+            winners=[]
+            winners.push(this.winner)
             JSON.stringify(winners)
-            storage.setItem(winners, winners);
+            localStorage.setItem(winners, winners);
         }
     }
-
+    //Just a pause, stops all code for 7 seconds
     async pause(){
-        await sleep(12000)
+        await sleep(7000)
     }
-    
-     timer(){
-        let timeleft = 10;
+    //The countdown that shows on screen
+    timer(){
+        let timeleft = 5;
         let answerTimer = setInterval(function(){
             if(timeleft < 0){
         
@@ -159,22 +149,19 @@ function startGame(){
                 document.getElementById("timer").innerText=timeleft
         
             }
-        
-            timeleft -= 1;
+        timeleft -= 1;
             
-        
         }, 1000);
-        }
-        
+    }
+        //Fetching the value of a div
         getGuess(div){
         return document.getElementById(div).value 
         }
-        
+        //Checks if guess is correct and renders info if the guess is too hight or low
         guessCheck(guess, correctNr){
             
             if (guess == correctNr){
                 return 1
-                
                 
             }else{
                 if(guess>correctNr)
@@ -188,11 +175,6 @@ function startGame(){
             }
         }
 }
-
-
-
-
-
 
 
 
